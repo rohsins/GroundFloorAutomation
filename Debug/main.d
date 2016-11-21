75e8ef934a0b07222fd34fc1c61d011f4a3028cc
Rohit Singh <rohsins@gmail.com>
2016-11-21 21:56:05 +0545

    version 1.0

--- Debug/main.d
+++ Debug/main.d
@@ -4,7 +4,7 @@ main.d main.o: .././main.cpp \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.106\include/avr/iom32.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.106\include/avr/iom16.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
@@ -13,13 +13,7 @@ main.d main.o: .././main.cpp \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
- .././LCDheader(rohsins).h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -31,7 +25,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.106\include/avr/iom32.h:
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.0.106\include/avr/iom16.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
@@ -50,15 +44,3 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
-
-.././LCDheader(rohsins).h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
--- main.cpp
+++ main.cpp
@@ -6,31 +6,53 @@
  */ 
 
 
-#define F_CPU 8000000UL
+#define F_CPU 1000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#include <string.h>
-#include "LCDheader(rohsins).h"
+//#include <string.h>
+//#include "LCDheader(rohsins).h"
+
+volatile uint8_t time = 0;
+
+ISR(TIMER1_OVF_vect) {
+	time++;
+	if (time >= 204) {
+		time = 0;
+		WDTCR = (1 << WDE);
+		while (1);
+	}
+}
+
+void OpenDoor(void) {
+	PORTC |= (1 << PD0);
+	_delay_ms(100);
+	PORTC &= ~(1 << PD0);
+}
 
 int main(void)
 {
-	DDRB = 0xFF;
+	//DDRB = 0xFF;
 	DDRA = 0x00;
-	DDRD |= (1 << 5) | (1 << 3) | (1 << 1);
+	DDRC = 0xFF;
+	//DDRD |= (1 << 5) | (1 << 3) | (1 << 1);
 	
 	PORTA = 0xFF;
+	PORTC = 0x00;
 	
-	lcd_initialize();
-	lcd(0x01,0);
-	_delay_ms(10);
-	lcd((char *)"Welcome");
+	sei();
+	TCCR1B |= (1 << CS12) | (1 << CS10); //prescalar /1024
+	TIMSK |= (1 << TOIE1);
+	TCNT1 = 0;
+	
+	//lcd_initialize();
+	//lcd(0x01,0);
+	//_delay_ms(10);
+	//lcd((char *)"Welcome");
 	
 	int i = 0;
 	int data[26];
-	//unsigned int FacilityCode = 0;
-	//unsigned int CardNumber = 0;
 	uint16_t FacilityCode = 0;
 	uint16_t CardNumber = 0;
 	while (1) {
@@ -52,18 +74,40 @@ int main(void)
 					CardNumber |= data[j] << (24-j);
 				}
 			}
-			lcd(0x80,0);
-			lcd((char *)"FC: ");
-			lcd(FacilityCode);
-			lcd((char *)" ");
 			if (FacilityCode == 188 && CardNumber == 34962) {
-				lcd((char *)"rohsins");
+				OpenDoor();
+			} else if (FacilityCode == 180 && CardNumber == 53992) {
+				OpenDoor();
+			} else if (FacilityCode == 220 && CardNumber == 53992) {
+				OpenDoor();
+			} else if (FacilityCode == 70 && CardNumber == 54248) {
+				OpenDoor();
+			} else if (FacilityCode == 93 && CardNumber == 54248) {
+				OpenDoor();
+			} else if (FacilityCode == 225 && CardNumber == 53992) {
+				OpenDoor();
+			} else if (FacilityCode == 147 && CardNumber == 53992) {
+				OpenDoor();
+			} else if (FacilityCode == 175 && CardNumber == 53736) {
+				OpenDoor();
+			} else if (FacilityCode == 54 && CardNumber == 54248) {
+				OpenDoor();
+			} else if (FacilityCode == 207 && CardNumber == 53992) {
+				OpenDoor();
+			} else {
+				//lcd(0x01, 0);
+				//lcd(0x80,0);
+				//lcd((char *)"FC: ");
+				//lcd(FacilityCode);
+				//lcd((char *)" ");
+				//lcd(0xC0,0);
+				//lcd((char *)"CN: ");
+				//lcd(CardNumber);
+				//lcd((char *)"   ");
 			}
-			lcd(0xC0,0);
-			lcd((char *)"CN: ");
-			lcd(CardNumber);
-			lcd((char *)"   ");
-			memset(data, 0, sizeof(data));
+			//memset(data, 0, sizeof(data));
+			FacilityCode = 0;
+			CardNumber = 0;
 			i = 0;
 		}
 	}
